7b7106edbb8798ae0f8f8bba905914027c25a327
StskV <S.tori10@yandex.by>
2026-06-10 21:12:07 +0300

    added headless params

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -40,8 +40,9 @@ public class BaseTest {
             options.addArguments("--disable-notifications");
             options.addArguments("--disable-popup-blocking");
             options.addArguments("--disable-infobars");
-            options.addArguments("--headless");
-
+            if(System.getProperty("headless", "true").equals("true")) {
+                options.addArguments("--headless");
+            }
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-gpu");
@@ -50,14 +51,18 @@ public class BaseTest {
             driver.manage().window().maximize();
         } else if (browser.equalsIgnoreCase("firefox")) {
             FirefoxOptions options = new FirefoxOptions();
-            options.addArguments("--headless");
+            if(System.getProperty("headless", "true").equals("true")) {
+                options.addArguments("--headless");
+            }
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-gpu");
             driver = new FirefoxDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
             EdgeOptions options = new EdgeOptions();
-            options.addArguments("--headless");
+            if(System.getProperty("headless", "true").equals("true")) {
+                options.addArguments("--headless");
+            }
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-gpu");
